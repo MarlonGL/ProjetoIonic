1fda811c0b2eef70d666bba7041ff15c2bd966c8
MarlonGL <MarlonGL@users.noreply.github.com>
2018-11-28 16:19:07 -0200

    platform funfando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,27 +26,42 @@ export class HomePage {
    score:number;
    scoreText;
    floor;
+   largura = 0;
+   altura = 0;
   constructor( public navCtrl: NavController, public plat:Platform) {
     //this.gameInstance = new Game(plat.width(), plat.height());
-    this.game = new Phaser.Game(plat.width(), plat.height(), Phaser.CANVAS, "game", {preload: this.preload,create: this.create,update: this.update, render:this.render});
-    console.log(plat.height());
-    console.log(plat.width());
-  }
-  preload(){
+    this.game = new Phaser.Game(plat.width(), plat.height(), Phaser.CANVAS, "game", {preload: preload,create: create,update: update, render:render});
+    
+    this.largura = plat.width();
+    this.altura = plat.height();
+    
+    function preload(){
 
+    }
+    function create(){
+      console.log('largura: ' + plat.width());
+      console.log('altura: ' + plat.height());
+      this.floor = new Phaser.Rectangle(0, 50, 800, 50);
+      var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+      //console.log(this.plat.height().toFixed());
+      this.scoreText = this.game.add.text(5, 360/3, 'Score: 0', style);
+      this.score = 0;
+    }
+    function update(){
+      //this.score += 10;
+      this.scoreText.text = 'Score: ' + this.score;
+    }
+    function render(){
+      //this.game.debug.geom(this.floor,'#0fffff');
+    }
   }
-  create(){
-    this.floor = new Phaser.Rectangle(0, 50, 800, 50);
-    var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-    //console.log(this.plat.height().toFixed());
-    this.scoreText = this.game.add.text(5, 360/3, 'Score: 0', style);
-    this.score = 0;
+
+  inicio(lar:number, alt:number){
+    this.largura = lar;
+    this.altura = alt;
   }
-  update(){
-    //this.score += 10;
-    this.scoreText.text = 'Score: ' + this.score;
+  getLarg() {
+    return 3;
   }
-  render(){
-    //this.game.debug.geom(this.floor,'#0fffff');
-  }
+  
 }
